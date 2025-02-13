a04f01a5f7dda39874a68d011e42ef1e81e91b88
mrkkjfrsn <marckie.js@gmail.com>
2025-02-13 23:47:04 +0800

    structuring

---
+++ resources/js/Pages/Landing/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Home = () => {
+	// dri pag sugod reignear naka plastar ng mga layout og components
+    return <div>Landing Page</div>;
+};
+
+export default Home;
